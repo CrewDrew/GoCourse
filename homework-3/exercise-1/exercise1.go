959f5b9d6db43ff4253e6cb877108b270ceb83ae
CrewDrew <kaa@211.ru>
2020-07-24 01:12:12 +0700

    Update exercise1.go

---
+++ homework-3/exercise-1/exercise1.go
@@ -0,0 +1,19 @@
+package main
+
+include ("fmt")
+
+type AutomobileDescribe struct {
+	Mark string
+	Year int
+	TrunkVolume float32
+	IsEngineRunning bool
+	IsWindowsOpen bool
+	TrunkVolumePercenr float32
+}
+
+func main() {
+
+
+
+
+}
