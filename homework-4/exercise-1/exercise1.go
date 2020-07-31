6c4c56e2947b4a398642ae641559b390ac9055eb
CrewDrew <kaa@211.ru>
2020-07-31 20:27:10 +0700

    Update exercise1.go

--- homework-4/exercise-1/exercise1.go
+++ homework-4/exercise-1/exercise1.go
@@ -1,25 +1,34 @@
 package main
 
-type chessBoardCell{
-	X, Y int
+type chessBoardCell struct {
+	x, y int
 }
 
 type ChessPiece interface {
 	MoveTo(int, int) bool
-	AvailableCells() []chessBoardCell 
+	AvailableCells() []chessBoardCell
 }
 
-type Pawn{
-	x, y int
+type Pawn struct {
+	position chessBoardCell
 }
 
-func (p *Pawn) MoveTo(x int, y int){
+func (p *Pawn) MoveTo(x int, y int) {
+	p.position.x = x
+	p.position.y = y
+}
+
+func (p Pawn) AvailableCells() []chessBoardCell {
+
+}
+
+func (p *Pawn) SetPosition(x int, y int) {
 	p.x = x
 	p.y = y
 }
 
-func (p Pawn) AvailableCells() []chessBoardCell {
-	
+func (p Pawn) GetPosition() {
+	return p
 }
 
 func main() {
