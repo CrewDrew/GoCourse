87b8eb0e4569cc223847e52c47d04928acfaa4e9
CrewDrew <kaa@211.ru>
2020-07-31 20:56:43 +0700

    Update exercise1.go

--- homework-4/exercise-1/exercise1.go
+++ homework-4/exercise-1/exercise1.go
@@ -1,34 +1,41 @@
 package main
 
-type chessBoardCell struct {
+// ChessBoardCell : coordinates of one cell of chess board
+type ChessBoardCell struct {
 	x, y int
 }
 
+// ChessPiece interface to chess figures
 type ChessPiece interface {
-	MoveTo(int, int) bool
-	AvailableCells() []chessBoardCell
+	SetPosition(int, int)
+	AvailablePositions() []ChessBoardCell
 }
 
+//Pawn : chess fugure
 type Pawn struct {
-	position chessBoardCell
+	position ChessBoardCell
 }
 
-func (p *Pawn) MoveTo(x int, y int) {
+// AvailablePositions return available cell for figure
+func (p Pawn) AvailablePositions() []ChessBoardCell {
+	var availablePositions []ChessBoardCell
+
+	if p.position.y+1 < 8 {
+		availablePositions = append(availablePositions, ChessBoardCell{x: p.position.x, y: p.position.y + 1})
+	}
+
+	return availablePositions
+}
+
+// SetPosition set the position to Pawn
+func (p *Pawn) SetPosition(x int, y int) {
 	p.position.x = x
 	p.position.y = y
 }
 
-func (p Pawn) AvailableCells() []chessBoardCell {
-
-}
-
-func (p *Pawn) SetPosition(x int, y int) {
-	p.x = x
-	p.y = y
-}
-
-func (p Pawn) GetPosition() {
-	return p
+// GetPosition return current chess position
+func (p Pawn) GetPosition() ChessBoardCell {
+	return p.position
 }
 
 func main() {
