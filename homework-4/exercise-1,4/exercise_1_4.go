f85afdace16377dcd4c1a28e9c2d99f14d2901e6
CrewDrew <kaa@211.ru>
2020-08-02 00:34:44 +0700

    Update exercise_1_4.go

--- homework-4/exercise-1,4/exercise_1_4.go
+++ homework-4/exercise-1,4/exercise_1_4.go
@@ -1,16 +1,34 @@
 package main
 
-import "errors"
+import (
+	"errors"
+	"fmt"
+	"math"
+)
+
+// MaxCoordinateValue is max value for coordinates of chess board
+const MaxCoordinateValue int = 8
 
 // ChessBoardCell : coordinates of one cell of chess board
 type ChessBoardCell struct {
 	x, y int
 }
 
+// ChessBoard is 2 dimensions array
+type ChessBoard [MaxCoordinateValue][MaxCoordinateValue]int
+
+// Init ChessBoard
+func (cb *ChessBoard) Init() {
+	for i := 0; i < MaxCoordinateValue; i++ {
+		for j := 0; j < MaxCoordinateValue; j++ {
+			cb[i][j] = j + 1
+		}
+	}
+}
+
 // ChessPiece interface to chess figures
 type ChessPiece interface {
-	SetPosition(ChessBoardCell)
-	AvailablePositions() ([]ChessBoardCell, error)
+	IsAvailablePosition(ChessBoardCell) (bool, error)
 }
 
 //Pawn : chess fugure
@@ -18,27 +36,26 @@ type Pawn struct {
 	position ChessBoardCell
 }
 
-// AvailablePositions return available cell for figure
-func (p Pawn) AvailablePositions() ([]ChessBoardCell, error) {
-	var availablePositions []ChessBoardCell
+// IsAvailablePosition return available cell for figure
+func (p Pawn) IsAvailablePosition(newPosition ChessBoardCell) (bool, error) {
 
 	if (p.position == ChessBoardCell{x: 0, y: 0}) {
-		errors.New("Невозможно получить доступные ячейки для фигуры вне доски")
+		return false, errors.New("Невозможно получить доступные ячейки для фигуры вне доски")
 	}
 
-	if p.position.y+1 <= 8 {
-		availablePositions = append(availablePositions, ChessBoardCell{x: p.position.x, y: p.position.y + 1})
+	if math.Abs(float64(p.position.y-newPosition.y)) <= 1.0 {
+		return true, nil
 	}
 
-	return availablePositions, nil
+	return false, errors.New("Выбранная позиция недоступна")
 }
 
 // SetPosition set the position to Pawn
 func (p *Pawn) SetPosition(newPosition ChessBoardCell) error {
-	if newPosition.x > 8 {
+	if newPosition.x > MaxCoordinateValue {
 		return errors.New("Координата X выходит за пределы доски")
 	}
-	if newPosition.y > 8 {
+	if newPosition.y > MaxCoordinateValue {
 		return errors.New("Координата Y выходит за пределы доски")
 	}
 
@@ -56,23 +73,22 @@ type Knight struct {
 	position ChessBoardCell
 }
 
-// AvailablePositions return available cell for figure
-func (p Knight) AvailablePositions() ([]ChessBoardCell, error) {
-	var availablePositions []ChessBoardCell
+// IsAvailablePosition return available cell for figure
+func (p Knight) IsAvailablePosition(newPosition ChessBoardCell) (bool, error) {
 
 	if (p.position == ChessBoardCell{x: 0, y: 0}) {
-		errors.New("Невозможно получить доступные ячейки для фигуры вне доски")
+		return false, errors.New("Невозможно получить доступные ячейки для фигуры вне доски")
 	}
 
-	if p.position.y+1 <= 8 {
-		availablePositions = append(availablePositions, ChessBoardCell{x: p.position.x, y: p.position.y + 1})
+	if math.Abs(float64((p.position.x-newPosition.x)*(p.position.y-newPosition.y))) == 2.0 {
+		return true, nil
 	}
 
-	return availablePositions, nil
+	return false, errors.New("Выбранная позиция недоступна")
 }
 
 // SetPosition set the position to Pawn
-func (p *Knight) setPosition(newPosition ChessBoardCell) {
+func (p *Knight) SetPosition(newPosition ChessBoardCell) {
 	p.position = newPosition
 }
 
@@ -81,26 +97,38 @@ func (p Knight) GetPosition() ChessBoardCell {
 	return p.position
 }
 
-// SetPiecePosition set the position to Pawn
-func SetPiecePosition(p ChessPiece, newPosition ChessBoardCell) error {
-	availablePositions, err := p.AvailablePositions()
+// AvailablePiecePositions return all available ositions for piece
+func AvailablePiecePositions(p ChessPiece, cb ChessBoard) []ChessBoardCell {
 
-	if err != nil {
-		return errors.New("Не удалось определить доступные ячейки")
-	}
+	var avalablePositions []ChessBoardCell
 
-	if availablePositions != nil {
-		for _, position := range availablePositions {
-			if position == newPosition {
-				p.SetPosition(newPosition)
-				return nil
+	for i := 0; i < len(cb); i++ {
+		for j := 0; j < len(cb[i]); j++ {
+			result, _ := p.IsAvailablePosition(ChessBoardCell{x: i + 1, y: j + 1})
+			if result {
+				avalablePositions = append(avalablePositions, ChessBoardCell{x: i + 1, y: j + 1})
 			}
 		}
-		return errors.New("Нельзя поместить фигуру в данную ячейку")
 	}
-	return errors.New("Нет доступных переходов")
+	return avalablePositions
 }
 
 func main() {
 
+	var chessBoard ChessBoard
+	var knightPiece Knight
+
+	chessBoard.Init()
+
+	knightPiece.SetPosition(ChessBoardCell{x: 1, y: 1})
+	fmt.Println("Начальная позиция коня ", knightPiece)
+
+	availablePositions := AvailablePiecePositions(knightPiece, chessBoard)
+	fmt.Println("Доступные из этого положения ходы: ", availablePositions)
+
+	knightPiece.SetPosition(ChessBoardCell{x: 5, y: 5})
+	fmt.Println("Начальная позиция коня ", knightPiece)
+
+	availablePositions = AvailablePiecePositions(knightPiece, chessBoard)
+	fmt.Println("Доступные из этого положения ходы: ", availablePositions)
 }
